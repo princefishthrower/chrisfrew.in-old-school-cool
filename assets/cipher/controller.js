478eeeab510a222f2598306f9bad148b9b1c88ca
Master Web Dev Chris <frewin.christopher@gmail.com>
2017-04-04 19:06:27 +0200

    better cipher

--- assets/cipher/controller.js
+++ assets/cipher/controller.js
@@ -1,23 +1,4 @@
-/**
- * Trigger a callback when the selected images are loaded:
- * @param {String} selector
- * @param {Function} callback
-  */
-var onImgLoad = function(selector, callback){
-    $(selector).each(function(){
-        if (this.complete || /*for IE 10-*/ $(this).height() > 0) {
-            callback.apply(this);
-        }
-        else {
-            $(this).on('load', function(){
-                callback.apply(this);
-            });
-        }
-    });
-};
-
-onImgLoad(".cipher", function(){
-  console.log("firing...");
+function onImageLoaded(){
   $(".cipher").addClass("cipherAnimation");
   $(".ring").addClass("ringAnimation");
   $(".fog1").addClass("fog1Animation fogAnimation");
@@ -30,6 +11,7 @@ onImgLoad(".cipher", function(){
   // set 5 second delay to match up with the dispatched animations
   setTimeout(function() {
     new Audio("wheelstart.mp3").play();
-  }, 5000);
-});
+  }, 8000);
+
+}
 
