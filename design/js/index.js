4a9e37cfb7e612513449c7494b62a8a46e9605e7
Master Web Dev Chris <frewin.christopher@gmail.com>
2017-04-11 21:21:47 +0200

    bachelor party site for james!

--- design/js/index.js
+++ design/js/index.js
@@ -1,6 +1,6 @@
-var _originalLeft;
-var _clicked;
-var iAnimationDuration = 2000;
+var _originalLeft; // original css 'left' value of page nav - first evaluated only after user clicks bio link
+var _clicked; // global var to tell if user has clicked link yet
+var _iAnimationDuration = 2000; // global animation grow/slide time in ms
 
 function onBioClick() {
   if (_clicked) {
@@ -13,21 +13,20 @@ function onBioClick() {
   $("#bioText").html("&lt;&lt; BIO");
 
   var bioHeight = $(".bio").css("height");
-  $(".page_nav").animate({left: $(window).width() / 2 - $(".page_nav").width() / 2}, {duration: iAnimationDuration, easing: "easeOutQuint"});
-  $(".bio").animate({height: 2000, opacity: 1}, {duration: iAnimationDuration, easing: "easeOutQuint"});
-  $(".biopic").animate({height: 154, opacity: 1}, {duration: iAnimationDuration, easing: "easeOutQuint"});
-  $("#background").fadeTo(iAnimationDuration, 0, "easeOutQuint");
+  $(".page_nav").animate({left: $(window).width() / 2 - $(".page_nav").width() / 2}, {duration: _iAnimationDuration, easing: "easeOutQuint"});
+  $(".bio").css("display", "block");
+  $(".bio").show().animate({height: 2000, opacity: 1}, {duration: _iAnimationDuration, easing: "easeOutQuint"});
+  $(".biopic").animate({height: 154, opacity: 1}, {duration: _iAnimationDuration, easing: "easeOutQuint"});
+  $("#background").fadeTo(_iAnimationDuration, 0, "easeOutQuint");
 }
 
 function onDoneClick() {
   _clicked = false;
   $("#bioText").html("BIO &gt;&gt;");
-
-  $(".page_nav").animate({left:_originalLeft}, {duration: iAnimationDuration, easing: "easeOutQuint"});
-  $(".bio").animate({height: 0, opacity: 0}, {duration: iAnimationDuration, easing: "easeOutQuint"});
-  $(".biopic").animate({height: 0, opacity: 0}, {duration: iAnimationDuration, easing: "easeOutQuint"});
-
-  $("#background").fadeTo(iAnimationDuration, 1, "easeOutQuint");
+  $(".page_nav").animate({left:_originalLeft}, {duration: _iAnimationDuration, easing: "easeOutQuint"});
+  $(".bio").animate({height: 0, opacity: 0}, {duration: _iAnimationDuration, easing: "easeOutQuint"}).hide(0);//
+  $(".biopic").animate({height: 0, opacity: 0}, {duration: _iAnimationDuration, easing: "easeOutQuint"});
+  $("#background").fadeTo(_iAnimationDuration, 1, "easeOutQuint");
 }
 
 $(window).resize(function(){
